db06b6fffe605b5971e9d135281fe9359156de23
B Vinoth <in0136@fbspl.com>
2022-01-10 15:07:17 +0530

    new changes

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     dots: true,
     appendDots: ".slider-dots",
     dotsClass: "dots",
-    autoplay: true,
+    // autoplay: true,
   });
 
   let hamberger = document.querySelector(".hamberger");
@@ -21,5 +21,36 @@ $(document).ready(function () {
 
   removeMenu = function () {
     mobileNav.classList.remove("open");
+  };
+
+  var modal = document.getElementById("project-modal");
+
+  openModal = function (project) {
+    modal.style.display = "block";
+    let list = document.querySelectorAll("." + project);
+    list.forEach((item) => {
+      item.classList.add("active");
+    });
+  };
+
+  var span = document.getElementsByClassName("close-modal")[0];
+
+  span.onclick = function () {
+    modal.style.display = "none";
+    let list = document.querySelectorAll(".modal-details");
+
+    list.forEach((item) => {
+      item.classList.remove("active");
+    });
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      let list = document.querySelectorAll(".modal-details");
+      list.forEach((item) => {
+        item.classList.remove("active");
+      });
+    }
   };
 });
