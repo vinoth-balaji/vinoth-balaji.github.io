b71b598c2106c12c3b7ebae41dce6d0b2525e193
B. Vinoth <in0136@flutura.com>
2022-08-04 21:28:18 +0530

    new changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,100 @@
 $(document).ready(function () {
   // console.log(window.screen.width);
 
+  function syntaxHighlight(json) {
+    // console.log(json)
+    json = json
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;");
+    return json.replace(
+      /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+      function (match) {
+        var cls = "number";
+        if (/^"/.test(match)) {
+          if (/:$/.test(match)) {
+            cls = "json-key";
+            match = match.substring(1, match.length - 2) + ": ";
+          } else {
+            cls = "json-string";
+            match = match.substring(1, match.length - 1);
+          }
+        } else if (/true|false/.test(match)) {
+          cls = "boolean";
+        } else if (/null/.test(match)) {
+          cls = "null";
+        }
+
+        // console.log(match, match.substring(1, match.length - 1));
+
+        return '<span class="' + cls + '">' + match + "</span>";
+      }
+    );
+  }
+
+  myDetails = {
+    From: "Vellore, Tamil Nadu",
+    "Graduated In": "Computer Science (2013-2017)",
+    "Working In": "Flutura Business Solutions, Bangalore",
+    Designation: "Associate Tech Lead - Full Stack",
+    "Years of Experience": "4.5+",
+  };
+
+  ewb = {
+    Organization: "Flutura Business Solutions, Bangalore",
+    Duration: "Jan 2020 - Till Date",
+    Designation: "Associate Tech Lead / Senior Programmer",
+    "Team Size": "8-10",
+  };
+
+  qp = {
+    Organization: "Flutura Business Solutions, Bangalore",
+    Duration: "June 2019 - Dec 2019",
+    Designation: "Programmer",
+    "Team Size": "6-8",
+  };
+
+  qd = {
+    Organization: "Flutura Business Solutions, Bangalore",
+    Duration: "Sep 2018 - May 2019",
+    Designation: "Programmer",
+    "Team Size": "10-12",
+  };
+
+  sc = {
+    Organization: "Flutura Business Solutions, Bangalore",
+    Duration: "Oct 2017 - Aug 2018",
+    Designation: "Programmer",
+    "Team Size": "4-6",
+  };
+
+  myDetails = JSON.stringify(myDetails, undefined, 4);
+  myDetails = syntaxHighlight(myDetails);
+  document.getElementById("myDetails").innerHTML =
+    '<span class="json-string json-title">Vinoth: </span>' + myDetails;
+
+  ewb = JSON.stringify(ewb, undefined, 4);
+  ewb = syntaxHighlight(ewb);
+  document.getElementById("ewb").innerHTML =
+    '<span class="json-string json-title">Engineering Workbench: </span>' + ewb;
+
+  qp = JSON.stringify(qp, undefined, 4);
+  qp = syntaxHighlight(qp);
+  document.getElementById("qp").innerHTML =
+    '<span class="json-string json-title">Quality Pulse: </span>' + qp;
+
+  qd = JSON.stringify(qd, undefined, 4);
+  qd = syntaxHighlight(qd);
+  document.getElementById("qd").innerHTML =
+    '<span class="json-string json-title">Quality Diagnostics and Prognostics: </span>' +
+    qd;
+
+  sc = JSON.stringify(sc, undefined, 4);
+  sc = syntaxHighlight(sc);
+  document.getElementById("sc").innerHTML =
+    '<span class="json-string json-title">PFEP (Plan for Every Part): </span>' +
+    sc;
+
   if (window.screen.width > 780) {
     $(".slider").slick({
       arrows: false,
