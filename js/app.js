5991a983c83f82d9fa3b61a27b719a36de2e5e4d
balajivinoth <b.vinoth@flutura.com>
2023-05-06 12:22:51 +0530

    new

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ function navHighlighter() {
   // Now we loop through sections to get height, top and ID values for each
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 150;
+    const sectionTop = current.offsetTop - 200;
     sectionId = current.getAttribute("id");
     // console.log(sections);
     // console.log(sectionId);
